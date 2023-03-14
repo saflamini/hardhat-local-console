73a8f3e3e179c2f2f01041b2726be5da8fa84a02
Sam Flamini <saflamini@gmail.com>
2023-03-13 20:42:59 -0500

    fix up by temp adding eth contracts

---
+++ ethereum-contracts/tasks/print-contract-sizes.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"/..
+
+find contracts/{superfluid,agreements,gov,utils} -name '*.sol'  | while read i;do
+    i=$(basename $i)
+    size=$(jq .bytecode build/contracts/${i/.sol/.json} | wc -c)
+    size=$(( $size / 2 ))
+    echo "$i : $size ($(( 24576 -$size )))"
+done
